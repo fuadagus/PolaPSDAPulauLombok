f280a5c245898cc0a14bafa34ff13b5fd3fc4295
fuadagus <fagussalim518@gmail.com>
2023-04-01 13:40:10 +0700

    menambahkan data spatial sekaligus routing

--- PolaPSDAPulauLombok/PolaPSDAPulauLombok.Designer.cs
+++ PolaPSDAPulauLombok/PolaPSDAPulauLombok.Designer.cs
@@ -29,49 +29,384 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PolaPSDAPulauLombok));
-            kryptonRibbon1 = new Krypton.Ribbon.KryptonRibbon();
-            kryptonRibbonTab1 = new Krypton.Ribbon.KryptonRibbonTab();
-            kryptonRibbonTab2 = new Krypton.Ribbon.KryptonRibbonTab();
-            kryptonRibbonGroup1 = new Krypton.Ribbon.KryptonRibbonGroup();
-            ((System.ComponentModel.ISupportInitialize)kryptonRibbon1).BeginInit();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            groupBox1 = new GroupBox();
+            button1 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button2 = new Button();
+            tabPage2 = new TabPage();
+            tabPage3 = new TabPage();
+            tabPage4 = new TabPage();
+            progressBar1 = new ProgressBar();
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            toolStripProgressBar1 = new ToolStripProgressBar();
+            splitContainer1 = new SplitContainer();
+            tabControl2 = new TabControl();
+            tabPage5 = new TabPage();
+            legend2 = new DotSpatial.Controls.Legend();
+            tabPage6 = new TabPage();
+            tabControl3 = new TabControl();
+            tabPage7 = new TabPage();
+            map1 = new DotSpatial.Controls.Map();
+            tabPage8 = new TabPage();
+            dataGridView1 = new DataGridView();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            groupBox1.SuspendLayout();
+            statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            tabControl2.SuspendLayout();
+            tabPage5.SuspendLayout();
+            tabControl3.SuspendLayout();
+            tabPage7.SuspendLayout();
+            tabPage8.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
-            // kryptonRibbon1
+            // tabControl1
             // 
-            kryptonRibbon1.InDesignHelperMode = true;
-            kryptonRibbon1.Name = "kryptonRibbon1";
-            kryptonRibbon1.RibbonTabs.AddRange(new Krypton.Ribbon.KryptonRibbonTab[] { kryptonRibbonTab1, kryptonRibbonTab2 });
-            kryptonRibbon1.SelectedTab = kryptonRibbonTab1;
-            kryptonRibbon1.Size = new Size(503, 143);
-            kryptonRibbon1.TabIndex = 0;
-            kryptonRibbon1.SelectedTabChanged += kryptonRibbon1_SelectedTabChanged;
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Controls.Add(tabPage3);
+            tabControl1.Controls.Add(tabPage4);
+            tabControl1.Dock = DockStyle.Top;
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(582, 128);
+            tabControl1.TabIndex = 0;
             // 
-            // kryptonRibbonTab1
+            // tabPage1
             // 
-            kryptonRibbonTab1.Groups.AddRange(new Krypton.Ribbon.KryptonRibbonGroup[] { kryptonRibbonGroup1 });
+            tabPage1.Controls.Add(groupBox1);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(574, 100);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "View";
+            tabPage1.UseVisualStyleBackColor = true;
             // 
-            // kryptonRibbonGroup1
+            // groupBox1
             // 
-            kryptonRibbonGroup1.Image = (Image)resources.GetObject("kryptonRibbonGroup1.Image");
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(button4);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Location = new Point(8, 0);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(563, 97);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // button1
+            // 
+            button1.Image = (Image)resources.GetObject("button1.Image");
+            button1.ImageAlign = ContentAlignment.TopCenter;
+            button1.Location = new Point(6, 22);
+            button1.Name = "button1";
+            button1.Size = new Size(73, 69);
+            button1.TabIndex = 0;
+            button1.Text = "Zoom In";
+            button1.TextAlign = ContentAlignment.BottomCenter;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Image = (Image)resources.GetObject("button3.Image");
+            button3.ImageAlign = ContentAlignment.TopCenter;
+            button3.Location = new Point(230, 22);
+            button3.Name = "button3";
+            button3.Size = new Size(72, 69);
+            button3.TabIndex = 2;
+            button3.Text = "Zoom Layer";
+            button3.TextAlign = ContentAlignment.BottomCenter;
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Image = (Image)resources.GetObject("button4.Image");
+            button4.ImageAlign = ContentAlignment.TopCenter;
+            button4.Location = new Point(155, 22);
+            button4.Name = "button4";
+            button4.Size = new Size(69, 66);
+            button4.TabIndex = 3;
+            button4.Text = "Zoom Extent";
+            button4.TextAlign = ContentAlignment.BottomCenter;
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Image = (Image)resources.GetObject("button2.Image");
+            button2.ImageAlign = ContentAlignment.TopCenter;
+            button2.Location = new Point(85, 22);
+            button2.Name = "button2";
+            button2.Size = new Size(64, 66);
+            button2.TabIndex = 1;
+            button2.Text = "Zoom Out";
+            button2.TextAlign = ContentAlignment.BottomCenter;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(574, 100);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Data";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            tabPage3.Location = new Point(4, 24);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Size = new Size(574, 100);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "Analysis";
+            tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            tabPage4.Location = new Point(4, 24);
+            tabPage4.Name = "tabPage4";
+            tabPage4.Size = new Size(574, 100);
+            tabPage4.TabIndex = 3;
+            tabPage4.Text = "Query";
+            tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Dock = DockStyle.Bottom;
+            progressBar1.Location = new Point(0, 400);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(582, 23);
+            progressBar1.TabIndex = 1;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1, toolStripProgressBar1 });
+            statusStrip1.Location = new Point(0, 378);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(582, 22);
+            statusStrip1.TabIndex = 2;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(465, 17);
+            toolStripStatusLabel1.Spring = true;
+            toolStripStatusLabel1.Text = "Ready";
+            toolStripStatusLabel1.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // toolStripProgressBar1
+            // 
+            toolStripProgressBar1.Name = "toolStripProgressBar1";
+            toolStripProgressBar1.Size = new Size(100, 16);
+            toolStripProgressBar1.Click += toolStripProgressBar1_Click;
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 128);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(tabControl2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(tabControl3);
+            splitContainer1.Size = new Size(582, 250);
+            splitContainer1.SplitterDistance = 194;
+            splitContainer1.TabIndex = 3;
+            // 
+            // tabControl2
+            // 
+            tabControl2.Controls.Add(tabPage5);
+            tabControl2.Controls.Add(tabPage6);
+            tabControl2.Dock = DockStyle.Fill;
+            tabControl2.Location = new Point(0, 0);
+            tabControl2.Name = "tabControl2";
+            tabControl2.SelectedIndex = 0;
+            tabControl2.Size = new Size(194, 250);
+            tabControl2.TabIndex = 1;
+            // 
+            // tabPage5
+            // 
+            tabPage5.Controls.Add(legend2);
+            tabPage5.Location = new Point(4, 24);
+            tabPage5.Name = "tabPage5";
+            tabPage5.Padding = new Padding(3);
+            tabPage5.Size = new Size(186, 222);
+            tabPage5.TabIndex = 0;
+            tabPage5.Text = "Table of Contents";
+            tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // legend2
+            // 
+            legend2.BackColor = Color.White;
+            legend2.ControlRectangle = new Rectangle(0, 0, 180, 216);
+            legend2.Dock = DockStyle.Fill;
+            legend2.DocumentRectangle = new Rectangle(0, 0, 187, 428);
+            legend2.HorizontalScrollEnabled = true;
+            legend2.Indentation = 30;
+            legend2.IsInitialized = false;
+            legend2.Location = new Point(3, 3);
+            legend2.MinimumSize = new Size(5, 5);
+            legend2.Name = "legend2";
+            legend2.ProgressHandler = null;
+            legend2.ResetOnResize = false;
+            legend2.SelectionFontColor = Color.Black;
+            legend2.SelectionHighlight = Color.FromArgb(215, 238, 252);
+            legend2.Size = new Size(180, 216);
+            legend2.TabIndex = 0;
+            legend2.Text = "legend2";
+            legend2.UseLegendForSelection = true;
+            legend2.VerticalScrollEnabled = true;
+            // 
+            // tabPage6
+            // 
+            tabPage6.Location = new Point(4, 24);
+            tabPage6.Name = "tabPage6";
+            tabPage6.Padding = new Padding(3);
+            tabPage6.Size = new Size(186, 222);
+            tabPage6.TabIndex = 1;
+            tabPage6.Text = "Base Map";
+            tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // tabControl3
+            // 
+            tabControl3.Controls.Add(tabPage7);
+            tabControl3.Controls.Add(tabPage8);
+            tabControl3.Dock = DockStyle.Fill;
+            tabControl3.Location = new Point(0, 0);
+            tabControl3.Name = "tabControl3";
+            tabControl3.SelectedIndex = 0;
+            tabControl3.Size = new Size(384, 250);
+            tabControl3.TabIndex = 1;
+            // 
+            // tabPage7
+            // 
+            tabPage7.Controls.Add(map1);
+            tabPage7.Location = new Point(4, 24);
+            tabPage7.Name = "tabPage7";
+            tabPage7.Padding = new Padding(3);
+            tabPage7.Size = new Size(376, 222);
+            tabPage7.TabIndex = 0;
+            tabPage7.Text = "Map";
+            tabPage7.UseVisualStyleBackColor = true;
+            // 
+            // map1
+            // 
+            map1.AllowDrop = true;
+            map1.BackColor = Color.White;
+            map1.CollisionDetection = false;
+            map1.Dock = DockStyle.Fill;
+            map1.ExtendBuffer = false;
+            map1.FunctionMode = DotSpatial.Controls.FunctionMode.None;
+            map1.IsBusy = false;
+            map1.IsZoomedToMaxExtent = false;
+            map1.Legend = legend2;
+            map1.Location = new Point(3, 3);
+            map1.Name = "map1";
+            map1.ProgressHandler = null;
+            map1.ProjectionModeDefine = DotSpatial.Controls.ActionMode.Prompt;
+            map1.ProjectionModeReproject = DotSpatial.Controls.ActionMode.Prompt;
+            map1.RedrawLayersWhileResizing = false;
+            map1.SelectionEnabled = true;
+            map1.Size = new Size(370, 216);
+            map1.TabIndex = 0;
+            map1.ZoomOutFartherThanMaxExtent = false;
+            map1.Load += map1_Load_1;
+            // 
+            // tabPage8
+            // 
+            tabPage8.Controls.Add(dataGridView1);
+            tabPage8.Location = new Point(4, 24);
+            tabPage8.Name = "tabPage8";
+            tabPage8.Padding = new Padding(3);
+            tabPage8.Size = new Size(376, 222);
+            tabPage8.TabIndex = 1;
+            tabPage8.Text = "Table Attribute";
+            tabPage8.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(3, 3);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(370, 216);
+            dataGridView1.TabIndex = 0;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
             // 
             // PolaPSDAPulauLombok
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(503, 338);
-            Controls.Add(kryptonRibbon1);
+            ClientSize = new Size(582, 423);
+            Controls.Add(splitContainer1);
+            Controls.Add(statusStrip1);
+            Controls.Add(progressBar1);
+            Controls.Add(tabControl1);
             Name = "PolaPSDAPulauLombok";
             Text = "Pola PSDA Pulau Lombok";
-            ((System.ComponentModel.ISupportInitialize)kryptonRibbon1).EndInit();
+            Load += PolaPSDAPulauLombok_Load;
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            groupBox1.ResumeLayout(false);
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            tabControl2.ResumeLayout(false);
+            tabPage5.ResumeLayout(false);
+            tabControl3.ResumeLayout(false);
+            tabPage7.ResumeLayout(false);
+            tabPage8.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Krypton.Ribbon.KryptonRibbon kryptonRibbon1;
-        private Krypton.Ribbon.KryptonRibbonTab kryptonRibbonTab1;
-        private Krypton.Ribbon.KryptonRibbonTab kryptonRibbonTab2;
-        private Krypton.Ribbon.KryptonRibbonGroup kryptonRibbonGroup1;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private TabPage tabPage2;
+        private TabPage tabPage3;
+        private TabPage tabPage4;
+        private ProgressBar progressBar1;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+        private ToolStripProgressBar toolStripProgressBar1;
+        private SplitContainer splitContainer1;
+        private GroupBox groupBox1;
+        private Button button4;
+        private TabControl tabControl2;
+        private TabPage tabPage5;
+        private DotSpatial.Controls.Legend legend2;
+        private TabPage tabPage6;
+        private TabControl tabControl3;
+        private TabPage tabPage7;
+        private DotSpatial.Controls.Map map1;
+        private TabPage tabPage8;
+        private DataGridView dataGridView1;
     }
 }
--- PolaPSDAPulauLombok/PolaPSDAPulauLombok.cs
+++ PolaPSDAPulauLombok/PolaPSDAPulauLombok.cs
@@ -1,14 +1,67 @@
+using DotSpatial.Data;
+using DotSpatial.Symbology;
+using Krypton.Ribbon;
+using Krypton.Toolkit;
+using PolaPSDAPulauLombok.Properties;
+using System.Security.Policy;
+using static DotSpatial.Data.AttributeCache;
+using System.IO;
+using System.Data;
+
 namespace PolaPSDAPulauLombok
 {
+
     public partial class PolaPSDAPulauLombok : Form
     {
+
+
+
         public PolaPSDAPulauLombok()
+
+
         {
+            String projectPath = Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).FullName).FullName).FullName;
+            String resourcesPath = projectPath + "\\resources";
+            String database = resourcesPath + "\\database";
+            String spatial = database + "\\Spatial";
+            //  MessageBox.Show(resourcesPath);
             InitializeComponent();
+            ILayer batasAdmin = map1.AddLayer(spatial + "\\Administrasi\\pulau_lombok.shp");
+            ILayer geologi = map1.AddLayer(spatial + "\\geologi\\pulau_lombok.shp");
         }
 
         private void kryptonRibbon1_SelectedTabChanged(object sender, EventArgs e)
         {
+
+        }
+
+        private void toolStripProgressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void map1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PolaPSDAPulauLombok_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kryptonRibbon1_SelectedTabChanged_1(object sender, EventArgs e)
+        {
+        }
+
+        private void map1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           // DataTable table = selectedLayer.Dataset.Datatable;
 
         }
     }
